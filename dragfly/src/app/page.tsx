5fad2abd5c359f1051af571675ade3f37842fb54
ceonmintoh <nmintoh@gmail.com>
2024-09-08 15:56:56 +0100

    Added Image Assets

--- dragfly/src/app/page.tsx
+++ dragfly/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import {useWeb3React} from '@web3-react/core';
+import {StargazerWeb3ReactConnector} from '@stardust-collective/web3-react-stargazer-connector';
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
+          src="/assets/images/genai_28.jpeg"
           alt="Next.js logo"
           width={180}
           height={38}
